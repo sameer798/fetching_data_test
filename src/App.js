8cf464c26b785c155921cdde8bc05794f1651cd6
sameer798 <sameershams.adi@gmailcom>
2024-08-26 17:34:34 +0530

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import {Button, Container, Row, Col} from 'react-bootstrap'
+import { useState } from 'react';
 import './App.css';
+import MoviesList from './components/MoviesList';
+
+// const DUMMY_MOVIES = [
+//   {id:1, title: "Some dummy movie", releaseDate: "2022-02-11", openingText : "This is opening text of movie"},
+//   {id:2, title: "Some dummy movie", releaseDate: "2022-02-11", openingText : "This is opening text of movie"},
+//   {id:3, title: "Some dummy movie", releaseDate: "2022-02-11", openingText : "This is opening text of movie"},
+//   {id:4, title: "Some dummy movie", releaseDate: "2022-02-11", openingText : "This is opening text of movie"},
+// ]
 
 function App() {
+
+  const [moviesState, setMoviesState] = useState([])
+  const fetchMoviesHandler = ()=>{
+    fetch('https://swapi.dev/api/films').then(response =>{
+     return response.json();
+    }).then(data=>{
+      const transformMoviesData = data.results.map(movieData=>{
+        return {
+          id : movieData.episode_id,
+          title : movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+
+        }
+      })
+      setMoviesState(transformMoviesData)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <Container className='mt-5'>
+        <Row className="justify-content-center">
+          <Col xs='auto'>
+              <Button variant='success' onClick={fetchMoviesHandler}>Fetch movies</Button>
+          </Col>
+        </Row>
+       </Container>
+       <MoviesList movies={moviesState}/>
+    </>
   );
 }
 
---
+++ src/components/MoviesList.js
@@ -0,0 +1,18 @@
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
+const MoviesList = (props) => {
+  return (
+    <ul className={classes["movies-list"]}>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MoviesList;
